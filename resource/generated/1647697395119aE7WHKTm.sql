f2d6d43a07eee557583bf63d6d37e5039b4866df
tin <tin@mail.com>
2022-03-19 20:43:32 +0700

    'test'

---
+++ resource/generated/1647697395119aE7WHKTm.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE IF NOT EXISTS `testBD`;
+
+USE `testBD`;
+
+CREATE TABLE IF NOT EXISTS `sinhvien` (
+  `ten` varchar(30),
+  PRIMARY KEY (``)
+);
+
+INSERT INTO `sinhvien`(`ten`) VALUES
+('Ninh Cát Yên Nga');
+
