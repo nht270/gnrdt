8e2c568291df90df2a4d46785c4ca1846f0e7770
tin <tin@mail.com>
2022-03-19 20:36:14 +0700

    'test'

--- controllers/sharedGeneratedData.js
+++ controllers/sharedGeneratedData.js
@@ -5,26 +5,17 @@ let path = require('path')
 let sharedGeneratedData = {
     index: async (req, res) => {
         let GeneratedFiles = await GeneratedFile.find({})
-        console.log({ GeneratedFile })
         let test = []
-        try {
-            GeneratedFiles.forEach(file => {
-                let fileName = file.fileName
-                let id = file._id
-                let filePath = path.join('resource', 'generated', file.fileName)
-                let link = 'resource/' + fileName
-                let content = ''
-                content = fs.readFileSync(filePath,
-                    { encoding: 'utf8', flag: 'r' })
+        GeneratedFiles.forEach(file => {
+            let fileName = file.fileName
+            let id = file._id
+            let filePath = path.join('resource', 'generated', file.fileName)
+            let link = 'resource/' + fileName
+            let content = fs.readFileSync(filePath,
+                { encoding: 'utf8', flag: 'r' })
 
-                test.push({ fileName, id, link, content })
-            })
-        } catch (e) {
-            console.log('Error read file: ' + filePath)
-            console.log('Cause: ', e)
-        } finally {
-            fs.close()
-        }
+            test.push({ fileName, id, link, content })
+        })
         console.log(test)
         res.render('sharedGeneratedData', { test })
     }
